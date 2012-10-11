bf1004e671f86675495f7e103e459f62a2fb8197
Shaan Shah <shahshaan@gmail.com>
2012-10-11 12:10:02 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psd::Application.config.secret_token = 'e0bb9415be0d16d20da8b2843189fa883f1ec7fc2fd08ebc3809da0f4a386c1ce9cadbf9623c7a7185e44f0b6dfb532c33a76e5034686d5ee1a12efba01db9f0'
